c00384bcec1210aab58fd0b3102cc9d4909b5c5c
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2018-09-20 13:33:40 +0200

    update

--- R/proLST.R
+++ R/proLST.R
@@ -6,29 +6,30 @@
 #' @param data.path Output data path for downloaded data.
 #' @import grDevices sp rgdal ncdf4
 #' @importFrom raster calc stack
+#' @importFrom lubridate is.Date
 #' @return One or multiple raster objects.
 #' @details {Downloads and pre-processes 
 #' \link[https://lpdaac.usgs.gov/dataset_discovery/modis/modis_products_table/mod11a2_v006]{MOD11A2} and 
 #' \link[https://lpdaac.usgs.gov/dataset_discovery/modis/modis_products_table/myd11a2_v006]{MYD11A2} data 
-#' for user specified \emph{tiles}. The data is downloaded from the 
-#' \link[https://ladsweb.modaps.eosdis.nasa.gov/]{LAADS DAAC server}. for each tile, the function downloads 
-#' the hdf files for closest in time to the elements in \emph{dates} and, for each hdf file, extract day and night 
-#' LST, applies quality information to each band and stores them as separate files names as "Date" + "tile" + 
-#' "collection" + "variable" + ".tif".}
+#' download for a given \emph{tile}. The function downloads the hdf file closest in time to \emph{date} 
+#' data is downloaded from the \link[https://ladsweb.modaps.eosdis.nasa.gov/]{LAADS DAAC server}. Then, it 
+#' extract day and night LST masked with Quanlity Control (QC) data and, when, TERRA and AQUA images are 
+#' available, merges both in a single file for each variable (i.e. "day" and "night"). The output files are 
+#' named as "Date" + "tile" + "product"/"combined" + "variable" + ".tif".}
 #' @export
 
 #-------------------------------------------------------------------------------------------------------------------------------#
 #-------------------------------------------------------------------------------------------------------------------------------#
 
 # land surface temperature
-proLST <- function(tile, dates, data.path) {
+proLST <- function(tile, date, data.path) {
   
 #-------------------------------------------------------------------------------------------------------------------------------#
 # 1. Check input variables
 #-------------------------------------------------------------------------------------------------------------------------------#
     
   if (!is.character(tile)) {stop('"tile" is not of a valid format')}
-  if (!is.Date(dates)) {stop('"dates" should be a date object')}
+  if (!is.Date(date)) {stop('"dates" should be a date object')}
   if (!dir.exists(data.path)) {stop('"data.path" is not a valid path')}
   
 #-------------------------------------------------------------------------------------------------------------------------------#
@@ -59,49 +60,37 @@ proLST <- function(tile, dates, data.path) {
 # 3. download and combine MODIS TERRA/AQUA
 #-------------------------------------------------------------------------------------------------------------------------------#
   
-  tmp <- lapply(1:length(ud), function(d) {
+  downloaded.files <- rbind(downloadLST(tile, ud[d], product="MOD11A2"), downloadLST(tile, ud[d], product="MYD11A2"))
+  i <- which(!is.na(downloaded.files$file)) # which files are available?
+  if (length(i) > 0) {
     
-    downloaded.files <- rbind(downloadLST(tile, ud[d], product="MOD11A2"), downloadLST(tile, ud[d], product="MYD11A2"))
-    i <- which(!is.na(downloaded.files$file)) # which files are available?
-    if (length(i) > 0) {
-      
-      downloaded.files <- downloaded.files[i,]
-      
-      ofiles <- paste0(data.path, downloaded.files$date, "_", tile, "_", downloaded.files$product)
-      
-      ofiles <- extractLST(downloaded.files$file, ofiles, delete.original=TRUE)
-      
-      # combine files (if available for TERRA and AQUA)
-      if (length(i) > 1) {
-        
-        ofile1 <- paste0(data.path, as.character(ud[d]), "_", tile, "_combined_lst-day.tif")
-        calc(stack(ofiles[c(1,3)]), mean, na.rm=TRUE, filename=ofile1, datatype="INT2U", overwrite=TRUE)
-        
-        rm(agg)
-        file.remove(ofiles[c(1,3)])
-        
-        ofile2 <- paste0(data.path, as.character(ud[d]), "_", tile, "_combined_lst-night.tif")
-        calc(stack(ofiles[c(2,4)]), mean, na.rm=TRUE, filename=ofile2, datatype="INT2U", overwrite=TRUE)
-        
-        rm(agg)
-        file.remove(ifile[c(2,4)])
-        
-        ofiles <- c(ofile1, ofile2)
-        
-      }
-      
-    } else {ofiles <- NULL}
+    downloaded.files <- downloaded.files[i,]
     
-    return(data.frame(date=ud[d], file.day=ofiles[1], file.night=ofiles[2], stringsAsFactors=FALSE))
+    ofiles <- paste0(data.path, downloaded.files$date, "_", tile, "_", downloaded.files$product)
     
-  })
+    ofiles <- extractLST(downloaded.files$file, ofiles, delete.original=TRUE)
+    
+    # combine files (if available for TERRA and AQUA)
+    if (length(i) > 1) {
+      
+      ofile1 <- paste0(data.path, as.character(ud[d]), "_", tile, "_combined_lst-day.tif")
+      calc(stack(ofiles[c(1,3)]), mean, na.rm=TRUE, filename=ofile1, datatype="INT2U", overwrite=TRUE)
+      
+      rm(agg)
+      file.remove(ofiles[c(1,3)])
+      
+      ofile2 <- paste0(data.path, as.character(ud[d]), "_", tile, "_combined_lst-night.tif")
+      calc(stack(ofiles[c(2,4)]), mean, na.rm=TRUE, filename=ofile2, datatype="INT2U", overwrite=TRUE)
+      
+      rm(agg)
+      file.remove(ifile[c(2,4)])
+      
+      ofiles <- c(ofile1, ofile2)
+      
+    }
+    
+  } else {ofiles <- NULL}
   
-#-------------------------------------------------------------------------------------------------------------------------------#
-# 4. return output as a data.frame specifying where the files are stored
-#-------------------------------------------------------------------------------------------------------------------------------#
-  
-  
-  return(do.call(rbind, tmp[[sapply(tmp, function(d) {!is.null(d)})]]))
-  
+  return(data.frame(date=ud[d], file.day=ofiles[1], file.night=ofiles[2], stringsAsFactors=FALSE))
   
 }
